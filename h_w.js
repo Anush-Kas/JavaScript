2314115fa96dfe58d0f6d2e115d8593b8a49542a
Anush <109114047+Anush-Kas@users.noreply.github.com>
2024-10-24 13:53:17 +0300

    Update h_w.js

--- h_w.js
+++ h_w.js
@@ -1,1 +1,25 @@
+function getTypeOfNumbers() {
+  const arr = [0, null, NaN, 0, 0, 1, 3, 4, 'str', 5]
 
+  let even = 0,
+      odd = 0,
+      zero = 0;
+
+  for(let i = 0; i < arr.lenght; i++) {
+    if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+      if (arr[i] === 0) {
+          zero += 1;
+      } else if (arr[i] % 2 === 0){
+          even += 1;
+      } else {
+          odd =+ 1;
+      }
+    }
+  }
+
+  console.log('четных элементов:', even)
+  console.log('нечетных элементов:', odd)
+  console.log('нулей:', zero)
+}
+
+getTypeOfNumbers()
